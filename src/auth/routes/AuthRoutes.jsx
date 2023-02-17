5a0c76b2a8ad2c38d8d9a93426b99377d44131ec
juan barrios <juanpablobarriosgrijalba@gmail.com>
2023-02-16 23:33:21 -0500

    register

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -4,12 +4,10 @@ import { LoginPage, RegisterPage } from "../pages";
 export const AuthRoutes = () => {
   return (
     <Routes>
-      <Route path="login" element={<LoginPage/>} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
 
-      <Route path="register" element={<RegisterPage/>} />
-
-<Route path='/*' element = { <Navigate to="/auth/login"/>} />
-
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
